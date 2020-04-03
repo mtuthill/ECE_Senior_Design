752768329ff209772470dea0a0a913ca91c5b985
radar <mwtuthill@crimson.ua.edu>
2020-04-02 21:32:06 -0400

    Adding python file

---
+++ pythonUI.py
@@ -0,0 +1,44 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+def window():
+	app = QApplication(sys.argv)
+	widget = QWidget()
+	
+	layout = QVBoxLayout(widget)
+
+	specLabel = QLabel(widget)
+	specLabel.setText("Spectrogram")
+	specLabel.setAlignment(Qt.AlignCenter)
+	font = QFont() 
+	font = specLabel.font()
+	font.setPointSize(30)
+	specLabel.setFont(font)
+
+	picture = QLabel(widget)
+	pixmap = QPixmap("/home/radar/Documents/Example Spectrograms/Earthquake/04010005_1582046023_Raw_0.png")
+	picture.setPixmap(pixmap)
+	picture.show()
+
+	fall = QLabel(widget)
+	fall.setText("Fall Detected")
+	fall.setAlignment(Qt.AlignCenter)
+	font = fall.font()
+	font.setPointSize(30)
+	fall.setStyleSheet("QLabel { background-color : red; color : black; }")
+	fall.setFont(font)
+
+	layout.addWidget(specLabel, 0, Qt.AlignCenter)
+	layout.addWidget(picture, 0, Qt.AlignCenter)
+	layout.addWidget(fall, 0, Qt.AlignCenter)
+
+	widget.setLayout(layout)
+
+	widget.setWindowTitle("Senior Design")
+	widget.show()
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+   window()
