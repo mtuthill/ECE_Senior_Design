8271f35b10fd6ce19c80763813276d87cee72146
Marc Tuthill <mwtuthill@crimson.ua.edu>
2020-10-03 17:23:01 -0500

    Adding another file that does not use the UI. Integrates spectrogram to feature with matlab with multiprocessing pipes

---
+++ qtUI/basicMainNoUI.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import classify
+import matlab.engine
+
+from multiprocessing import Process, Pipe
+from os import listdir
+from os.path import isfile, join
+from time import sleep
+
+def readXbee():
+    #readXbee
+    for i in range(40):
+        print("read")
+        sleep(1.6548666)
+
+    #sendXbee to callMatlab
+
+def getFeaturesFromSpectrogram(postFeatureConn):
+    #need to pass in data from xbees
+
+    #get filenames for spectrograms
+    path = "../../ECE_Senior_Design_Data/nonFallSpectrograms/05_Walking_towards_radar/"
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+
+    eng = matlab.engine.start_matlab()
+    for file in onlyfiles:
+        returned = eng.dctFromPng(path + file)
+        postFeatureConn.send(returned)
+        print("Sent the message: {}".format(returned))
+        sleep(1)
+
+    eng.quit()
+
+def callClassification(postFeatureConn):
+    while 1:
+        msg = postFeatureConn.recv()
+        print("Recieved the message: {}".format(msg))
+        #classify
+
+
+if __name__ == "__main__":
+    matlabSender, classificationReciever = Pipe()
+
+    p1 = Process(target = getFeaturesFromSpectrogram, args = (matlabSender,))
+    p2 = Process(target = callClassification, args = (classificationReciever,))
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
