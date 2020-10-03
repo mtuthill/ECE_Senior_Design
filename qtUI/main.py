888d238d2617b453319d75b95ad7ddd9ac94c769
Marc Tuthill <mwtuthill@crimson.ua.edu>
2020-10-02 22:29:41 -0500

    Adding function skeleton to read from xbee running synchronously

--- qtUI/main.py
+++ qtUI/main.py
@@ -8,7 +8,17 @@ from ui_mainwindow import Ui_MainWindow
 from multiprocessing import Pool
 from time import sleep
 
+def readXbee():
+    #readXbee
+    for i in range(40):
+        print("read")
+        sleep(1.6548666)
+
+    #sendXbee to callMatlab
+
 def callMatlab(num):
+    #need to pass in data from xbees
+
     print("here")
     eng = matlab.engine.start_matlab()
     for i in range(0, 60):
@@ -27,17 +37,11 @@ def startUI():
 
 
 if __name__ == "__main__":
-
-    #read in values from Xbee, might need to run in parallel?
-
-
-    #organize data as needed
-
-
     #sync calls
     pool = Pool()
     result1 = pool.apply_async(callMatlab, 20)
     result2 = pool.apply_async(startUI)
+    result3 = pool.apply_async(readXbee)
     callMatlab(10)
 
 
