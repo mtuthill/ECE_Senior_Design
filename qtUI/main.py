cf34d8d3914b20338b6acf13099a3450a438c279
Marc Tuthill <mwtuthill@crimson.ua.edu>
2020-10-19 16:58:19 -0500

    Adding integration of reading mmWaveData

--- qtUI/main.py
+++ qtUI/main.py
@@ -10,20 +10,29 @@ from time import sleep
 from os import listdir
 from os.path import isfile, join
 
-def readXbee():
-    #readXbee
-    for i in range(40):
-        sleep(1.6548666)
-
-    #sendXbee to callMatlab
-
 def callMatlab():
-    #need to pass in data from xbees
+    #get binary data
+    dataPath = "./"
+    onlyfiles = [f for f in listdir(dataPath) if isfile(join(dataPath, f))]
+    filename = 'empty_target_Raw_0.bin'
+    while (not(filename in onlyfiles)):
+        print("file checked for")
+        sleep(10)
+        onlyfiles = [f for f in listdir(dataPath) if isfile(join(dataPath, f))]
+
+    print("File found")
+
+    f=open(filename, "rb")
+    num=list(f.read())
+    f.close()
+
+    #get spectrogram
 
     #get filenames for spectrograms
     path = "../../ECE_Senior_Design_Data/nonFallSpectrograms/05_Walking_towards_radar/"
     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
+    #get features
     eng = matlab.engine.start_matlab()
     for file in onlyfiles:
         returned = eng.dctFromPng(path + file)
@@ -44,7 +53,6 @@ if __name__ == "__main__":
     pool = Pool()
     result1 = pool.apply_async(callMatlab)
     result2 = pool.apply_async(startUI)
-    result3 = pool.apply_async(readXbee)
     callMatlab()
 
 
