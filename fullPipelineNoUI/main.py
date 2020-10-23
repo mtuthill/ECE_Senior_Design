a129c406ed3b868da122b12afb37c4894c09db1b
Marc Tuthill <mwtuthill@crimson.ua.edu>
2020-10-23 12:09:04 -0500

    Working on multiprocessing the data and matlab calls

--- fullPipelineNoUI/main.py
+++ fullPipelineNoUI/main.py
@@ -2,57 +2,70 @@ import sys
 import os
 import matlab.engine
 import joblib
+import multiprocessing
 
 from time import perf_counter
-from os.path import isfile
+from os.path import isfile, getsize
 
-#start matlab
-eng = matlab.engine.start_matlab()
+def findResult(startBit, lengthToRead, f, eng, procNum):
+	#read data starting at specified bit for a certain length
+	f.seek(startBit)
+	rawData = list(f.read(lengthToRead))
 
-#load classifier that is pretrained
-classifierFile = "storedTestSVM.sav"
-classifier = joblib.load(classifierFile)
+	#get spectrogram
+	#get filenames for spectrograms (temporay until actual spectrogram)
+	path = r"../../ECE_Senior_Design_Data/nonFallSpectrograms/0_Walking_towards_radar/"
+	dataFile = r"04000000_1574696114_Raw_0.png"
 
-#find data
-dataPath = r"./"
-filename = r"target_Raw_0.bin"
-fileSizeFlag = 0
-startedTimerFlag = 0
-startTime = perf_counter()
+	#get features
+	features = eng.dctFromPng(path + dataFile)
 
-#print("Checking for file")
-while ((not(isfile(dataPath + filename))) and fileSizeFlag == 0):
-    if (isfile(dataPath + filename)):
-    	if (os.stat(filename).st_size > 76799000):
-        	fileSizeFlag = 1
+	#get classification result
+	result = classifier.predict(features)
+	
+	print("Result {} = {}".format(procNum, result1))
 
-print("Time from start to finding binary data file")
-print(perf_counter() - startTime)
 
-#read file
-f = open(filename, "rb")
-rawData = list(f.read())
-print("Time from start to reading binary data")
-print(perf_counter() - startTime)
+	return result
 
-#get spectrogram
 
-#get filenames for spectrograms (temporay until actual spectrogram)
-path = r"../../ECE_Senior_Design_Data/nonFallSpectrograms/0_Walking_towards_radar/"
-dataFile = r"04000000_1574696114_Raw_0.png"
+if __name__ == "__main__":
+	#start matlab
+	eng = matlab.engine.start_matlab()
 
-#get features
-features = eng.dctFromPng(path + dataFile)
-print("Time from start to getting features from spectrogram")
-print(perf_counter() - startTime)
+	#load classifier that is pretrained
+	classifierFile = "storedTestSVM.sav"
+	classifier = joblib.load(classifierFile)
 
-#get classification result
-result = classifier.predict(features)
+	#find data
+	dataPath = r"./"
+	filename = r"target_Raw_0.bin"
+	fileSizeFlag = 0
+	startedTimerFlag = 0
+	startTime = perf_counter()
 
-#stop timer
-endTime = perf_counter()
-print("Total time in secs = ")
-print(endTime - startTime)
+	#find file
+	while ((not(isfile(dataPath + filename))) and fileSizeFlag == 0):
+	    if (isfile(dataPath + filename)):
+	    	if (os.stat(filename).st_size > 76799000):
+	        	fileSizeFlag = 1
 
-f.close()
-eng.quit()
+	#open file
+	f = open(filename, "rb")
+	fileSize = getsize(filename)
+
+	#find results
+	pool = multiprocessing.Pool()
+	result1 = pool.apply_async(findResult, args = (0, fileSize * 0.5, f, eng, 0))
+	result2 = pool.apply_async(findResult, args = (fileSize * 0.25, fileSize * 0.5, f, eng, 1))
+	result3 = pool.apply_async(findResult, args = (fileSize * 0.5, fileSize * 0.5, f, eng, 2))
+	pool.close()
+	pool.join()
+
+	#stop timer
+	endTime = perf_counter()
+	print("Total time in secs = ")
+	print(endTime - startTime)
+
+	f.close()
+	eng.quit()
