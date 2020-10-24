84ff5a55a99553f58ad4334778b3b12cab43e828
Marc Tuthill <mwtuthill@crimson.ua.edu>
2020-10-24 14:57:16 -0500

    Full multiprocessing in place for each fragment and matlab calls

--- fullPipelineNoUI/main.py
+++ fullPipelineNoUI/main.py
@@ -2,12 +2,48 @@ import sys
 import os
 import matlab.engine
 import joblib
-import multiprocessing
 
+from multiprocessing import Pool
 from time import perf_counter
 from os.path import isfile, getsize
 
-def findResult(startBit, lengthToRead, f, eng, procNum):
+def findResult(num):
+	#annoyingly, matlab engine api does not like to pass an engine object to a different process
+	eng = matlab.engine.start_matlab()
+
+	#load classifier that is pretrained
+	classifierFile = "storedTestSVM.sav"
+	classifier = joblib.load(classifierFile)
+
+	#find data
+	dataPath = r"./"
+	filename = r"target_Raw_0.bin"
+	fileSizeFlag = 0
+	startedTimerFlag = 0
+
+	startTime = perf_counter()
+
+	#find file
+	while ((not(isfile(dataPath + filename))) and fileSizeFlag == 0):
+	    if (isfile(dataPath + filename)):
+	    	if (os.stat(filename).st_size > 76799000):
+	        	fileSizeFlag = 1
+
+	#get file size
+	fileSize = getsize(filename)
+
+	#openfile
+	f = open(filename, "rb")
+
+	#find what bits to read
+	if (num == 1):
+		startBit = 0
+	elif (num == 2):
+		startBit = int(fileSize * 0.25)
+	else:
+		startBit = int(fileSize * 0.5)
+	lengthToRead = int(fileSize * 0.5)
+
 	#read data starting at specified bit for a certain length
 	f.seek(startBit)
 	rawData = list(f.read(lengthToRead))
@@ -22,50 +58,20 @@ def findResult(startBit, lengthToRead, f, eng, procNum):
 
 	#get classification result
 	result = classifier.predict(features)
+
+	f.close()
+
+	endTime = perf_counter()
+	print("Total time in secs for fragment {} = {}".format(num, endTime - startTime))
 	
-	print("Result {} = {}".format(procNum, result1))
-
-
 	return result
 
 
 if __name__ == "__main__":
-	#start matlab
-	eng = matlab.engine.start_matlab()
+	with Pool(3) as pool:
+		results = pool.map(findResult, [1, 2, 3])
+	
+	#pool.close()
+	#pool.join()
 
-	#load classifier that is pretrained
-	classifierFile = "storedTestSVM.sav"
-	classifier = joblib.load(classifierFile)
-
-	#find data
-	dataPath = r"./"
-	filename = r"target_Raw_0.bin"
-	fileSizeFlag = 0
-	startedTimerFlag = 0
-	startTime = perf_counter()
-
-	#find file
-	while ((not(isfile(dataPath + filename))) and fileSizeFlag == 0):
-	    if (isfile(dataPath + filename)):
-	    	if (os.stat(filename).st_size > 76799000):
-	        	fileSizeFlag = 1
-
-	#open file
-	f = open(filename, "rb")
-	fileSize = getsize(filename)
-
-	#find results
-	pool = multiprocessing.Pool()
-	result1 = pool.apply_async(findResult, args = (0, fileSize * 0.5, f, eng, 0))
-	result2 = pool.apply_async(findResult, args = (fileSize * 0.25, fileSize * 0.5, f, eng, 1))
-	result3 = pool.apply_async(findResult, args = (fileSize * 0.5, fileSize * 0.5, f, eng, 2))
-	pool.close()
-	pool.join()
-
-	#stop timer
-	endTime = perf_counter()
-	print("Total time in secs = ")
-	print(endTime - startTime)
-
-	f.close()
-	eng.quit()
+	print(results)
